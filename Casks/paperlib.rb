8f1c1a44216553f235973aa5369c08883af5e079
GeoffreyChen777 <geoffreychen777@gmail.com>
2024-03-13 16:32:42 +0000

    Update paperlib.rb

--- Casks/paperlib.rb
+++ Casks/paperlib.rb
@@ -1,27 +1,27 @@
 cask "paperlib" do
-  version "3.0.0-beta.11"
+  version "3.0.0"
   sha256 :no_check
 
   arch arm: "arm", intel: "x86-64"
-  channel = on_arch_conditional arm: "arm_dev_channel", intel: "dev_channel"
-
-  url "https://paperlib.app/distribution/#{channel}/Paperlib%20Beta_#{version}.dmg",
+  channel = on_arch_conditional arm: "electron-mac-arm", intel: "electron-mac"
+  
+  url "https://paperlib.app/distribution/#{channel}/Paperlib_#{version}.dmg",
       verified: "paperlib.app/distribution/#{channel}"
-  name "Paperlib Beta"
-  desc "A simple academic paper management tool. (Beta version)"
+  name "Paperlib"
+  desc "A simple academic paper management tool."
   homepage "https://paperlib.app/en/"
 
   livecheck do
-    url "https://paperlib.app/distribution/#{channel}/beta-mac.yml"
+    url "https://paperlib.app/distribution/#{channel}/latest-mac.yml"
     strategy :electron_builder
   end
 
-  app "Paperlib Beta.app"
+  app "Paperlib.app"
 
-  uninstall quit: "dev.beta.paperlib.app"
+  uninstall quit: "dev.paperlib.app"
 
   zap trash: [
-    "~/Library/Application Support/paperlib-beta/",
-    "~/Library/Application Support/Caches/paperlib-beta-updater/",
+    "~/Library/Application Support/paperlib/",
+    "~/Library/Application Support/Caches/paperlib-updater/",
   ]
 end
